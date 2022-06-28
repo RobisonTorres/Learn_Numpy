fe1be18dca2980cbe06149b24f2861be90e379bc
Robison Torres <robison.torres21@gmail.com>
2022-06-28 11:57:50 -0300

    Initial Commit

--- 03 - Slicing.py
+++ 03 - Slicing.py
@@ -14,3 +14,8 @@ print(arr[1, 1:4])  # Output - [7 8 9]
 # From both elements, return index 2:
 arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
 print(arr[0:2, 2])  # Output - [3 8]
+
+# Changing elements in an array:
+a = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+a[:, 2] = 20  # Change in both arrays.
+print(a)
--- test_free.py
+++ test_free.py
@@ -2,3 +2,7 @@ import numpy as np
 print('Keep it clean.')
 print()
 
+a = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+
+a[:, 2] = 20
+print(a)
